e49fc9cfcdf670267968f5d228189d7b55d5380a
Sean Newman <sxn6296@rit.edu>
2018-10-04 17:55:55 -0400

    Add ISTS 14 website
    The ISTS 14 website files were refactored and the routes were re-added,
    so it can still be viewed.

--- project/views/__init__.py
+++ project/views/__init__.py
@@ -3,9 +3,15 @@ This module sets up the views for easy configuration in the application
 factory.
 """
 # Internal imports
-from project.views import root
+from project.views import (
+    root,
+    fourteen,
+    fifteen,
+)
 
-# Pack views for registration
+# Pack views for registration.
 views = [
     root.bp,
+    fourteen.bp,
+#    fifteen.bp,
 ]
---
+++ project/views/fourteen.py
@@ -0,0 +1,38 @@
+"""
+This module configures the routes to serve the ISTS 14 website.
+"""
+# External library imports
+from flask import Blueprint, render_template
+
+##
+#   Attributes
+##
+bp = Blueprint(
+    'fourteen', __name__,
+    static_folder='static/14',
+    url_prefix='/14',
+)
+
+
+##
+#   Routes
+##
+@bp.route('/')
+@bp.route('/index')
+def index():
+    return render_template('14/index.html', title='ISTS', ists_year='14', year='2016')
+
+
+@bp.route('/about')
+def about():
+    return render_template('14/about.html', title='About:ISTS', ists_year='14')
+
+
+@bp.route('/sponsors')
+def sponsors():
+    return render_template('14/sponsors.html', title='sponsors', ists_year='14')
+
+
+@bp.route('/news')
+def news():
+    return render_template('14/news.html', title='news', ists_year='14')
