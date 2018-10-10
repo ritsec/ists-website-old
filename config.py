42bdcc17556170e4629dca2120f9d196783304ab
Sean Newman <sxn6296@rit.edu>
2018-10-10 02:09:47 -0400

    Screw it.
    I don't care any more, man.  Just work.

--- config.py
+++ config.py
@@ -4,25 +4,25 @@ configuration values are the defaults for the development deployment, and
 should be changed.
 """
 # Unique, secure secret key for the Flask web server.
-SECRET_KEY = 'dev'
+SECRET_KEY = 'ExAwOlw3PwNWnZROXAEJaOFr1/ezy4C6LOASg7j58pU='
 
 # The number of the upcoming ISTS event.
-ISTS_NUMBER = '00'
+ISTS_NUMBER = '17'
 
 # The year in which the next ISTS will happen.
-YEAR = '1970'
+YEAR = '2019'
 
 # The month in which the next ISTS will happen.
-MONTH = 'January'
+MONTH = 'February'
 
 # The day of the month of the set-up day for the next ISTS.
-SETUP_DAY = '1'
+SETUP_DAY = '22'
 
 # The day of the month of the first competition day for the next ISTS.
-FIRST_DAY = '2'
+FIRST_DAY = '23'
 
 # The day of the month of the second competition day for the next ISTS.
-SECOND_DAY = '3'
+SECOND_DAY = '24'
 
 # The main room in which the next ISTS will be held.
 ROOM = 'USC-1600'
@@ -31,10 +31,10 @@ ROOM = 'USC-1600'
 BLUE_REG_OPEN = True
 
 # The link for blue team registration for the next ISTS.
-BLUE_REG_LINK = 'https://google.com'
+BLUE_REG_LINK = 'blueteam-ists.ritsec.club'
 
 # The link for white team registration for the next ISTS.
-WHITE_REG_LINK = 'https://google.com'
+WHITE_REG_LINK = 'whiteteam.ritsec.club'
 
 # The contact email for more information about non-RIT blue team information.
 CONTACT_EMAIL = 'ritsecclub@gmail.com'
